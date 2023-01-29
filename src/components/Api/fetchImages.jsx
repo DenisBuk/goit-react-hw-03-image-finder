e4e528d4934b714e11d61117f0ff756e0cb25cd5
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-01-29 19:48:28 +0200

    fetch

--- src/components/Api/fetchImages.jsx
+++ src/components/Api/fetchImages.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 export const fetchImages = async (inputValue, pageNr) => {
-    const response = await axios.get(`/?q=${inputValue}&page={pageNr}&key=33208866-b30484cdea6ad4b23b3d7b77d&image_type=photo&orientation=horizontal&per_page=12`);
+    const response = await axios.get(`/?q=${inputValue}&page=${pageNr}&key=33208866-b30484cdea6ad4b23b3d7b77d&image_type=photo&orientation=horizontal&per_page=12`);
     return response.data.hits.map(image => {
         return {
             id: image.id,
