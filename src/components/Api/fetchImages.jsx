fbbf19e91642ae20a0633384618934e04e3cf278
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-01-29 22:03:10 +0200

    Api

--- src/components/Api/fetchImages.jsx
+++ src/components/Api/fetchImages.jsx
@@ -4,13 +4,15 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 
 export const fetchImages = async (inputValue, pageNr) => {
     const response = await axios.get(`/?q=${inputValue}&page=${pageNr}&key=33208866-b30484cdea6ad4b23b3d7b77d&image_type=photo&orientation=horizontal&per_page=12`);
-    return response.data.hits.map(image => {
+   
+    return response.data;
+};
+    
+/* return response.data.hits.map(image => {
         return {
             id: image.id,
             webformatURL: image.webformatURL,
             largeImageURL: image.largeImageURL,
-            tags: image.tags,
-        };
-    });
-};
+            tags: image.tags,*/
+       
 
